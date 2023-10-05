44cc98e1d68af5534f1e0693d22d5d5e39f83213
Thibault Giraudon <tgiraudo@z3r7p3.42lyon.fr>
2023-10-05 13:59:06 +0200

    Connect with hexchat but dont work with nc

--- srcs/utils/split.cpp
+++ srcs/utils/split.cpp
@@ -6,13 +6,13 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 08:08:45 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/10/05 08:12:53 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/10/05 13:55:16 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "irc.hpp"
 
-std::vector<std::string> split(std::string s, char delimiter)
+std::vector<std::string> split(std::string s, std::string delimiter)
 {
     std::vector<std::string>    split_str;
     std::string                 token;
@@ -20,10 +20,21 @@ std::vector<std::string> split(std::string s, char delimiter)
     
     while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
         token = s.substr (pos_start, pos_end - pos_start);
-        pos_start = pos_end + 1;
+        pos_start = pos_end + delimiter.size();
         split_str.push_back (token);
     }
 
     split_str.push_back (s.substr (pos_start));
     return split_str;
+}
+
+std::string erasechar(std::string s, char erased) 
+{
+    size_t  pos;
+    std::cout << "before: " << s << std::endl;
+    while ((pos= s.find(erased, 0)) != std::string::npos) {
+        s.erase(pos, 1);
+    }
+    std::cout << "after: " << s << std::endl;
+    return (s);
 }
