9cdbd1709b6ce6204087e640bd89e45976644542
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-10-10 14:26:53 +0200

    add cap norm

--- srcs/command/cap.cpp
+++ srcs/command/cap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cap.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpupier <lpupier@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 09:45:50 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/10/10 09:28:23 by lpupier          ###   ########.fr       */
+/*   Updated: 2023/10/10 14:26:41 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ bool	Server::_cap(int fd)
 		ERR_ALREADYREGISTERED(this->_users[fd]);
 		return (false);
 	}
-	RPL_CMD(this->_users[fd], "CAP", "Welcome to IRC, to login use: " \
+	RPL_CMD(this->_users[fd], "CAP", "Welcome to IRC, to login use: "\
 		"\nPASS <password>\nNICK <nickname>\nUSER <name> 0 * :realname\n");
 	return (true);
 }
