8c48070435ea82efb0d78955c2897c96ab50d6dd
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-10-10 14:13:49 +0200

    fix split \r

--- srcs/utils/split.cpp
+++ srcs/utils/split.cpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 08:08:45 by tgiraudo          #+#    #+#             */
-/*   Updated: 2023/10/10 13:41:27 by elias            ###   ########.fr       */
+/*   Updated: 2023/10/10 14:13:07 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ std::vector<std::string> split(std::string str, char delimiter)
 {
 	// TODO add secu quand on a "NICK         " -> [NICK][][][] 
 
+	str.erase(std::remove(str.begin(), str.end(), '\r'), str.end());
     std::vector<std::string> splitString;
     std::stringstream ss(str);
     std::string token;
@@ -27,3 +28,18 @@ std::vector<std::string> split(std::string str, char delimiter)
     }
 	return (splitString);
 }
+
+// std::vector<std::string> split(std::string str, std::string delimiter)
+// // {
+// // 	std::vector<std::string>    split_str;
+// // 	std::string					token;
+// // 	size_t						pos_start = 0, pos_end;
+	
+// // 	while ((pos_end = str.find(delimiter, pos_start)) != std::string::npos) {
+// // 		token = str.substr (pos_start, pos_end - pos_start);
+// // 		pos_start = pos_end + delimiter.size();
+// // 		split_str.push_back (token);
+// // 	}
+// // 	split_str.push_back (str.substr (pos_start));
+// // 	return split_str;
+// // }
